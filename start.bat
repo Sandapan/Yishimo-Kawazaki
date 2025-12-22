b65dac66c4376ce208fcd46c3c9d7b2615f6dd23
Sandapan <94439161+Sandapan@users.noreply.github.com>
2025-12-22 19:25:27 +0100

    edit

--- start.bat
+++ start.bat
@@ -13,8 +13,8 @@ start "Frontend Server" cmd /k "cd frontend && npm start"
 
 echo.
 echo Les deux serveurs sont lances sur le reseau !
-echo Backend: http://192.168.1.98:8000
-echo Frontend: http://192.168.1.98:3000
+echo Backend: http://192.168.1.99:8000
+echo Frontend: http://192.168.1.99:3000
 echo.
 echo Fermez cette fenetre pour garder les serveurs actifs.
 pause
